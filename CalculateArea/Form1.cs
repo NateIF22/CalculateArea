336cb16cece1ab689d58a494af770fa19c87c14c
NateIF22 <nathanfisnothere@gmail.com>
2026-01-20 10:10:31 -0800

    feat; add better validation.

--- CalculateArea/Form1.cs
+++ CalculateArea/Form1.cs
@@ -19,16 +19,40 @@ namespace CalculateArea
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            if (!Validators.IsMinimumLength(txtWidth.Text, 1)||
-                !Validators.IsMinimumLength(txtHeight.Text, 1))
+            //if (!Validators.IsMinimumLength(txtWidth.Text, 1)||
+            //    !Validators.IsMinimumLength(txtHeight.Text, 1))
+            //{
+            //    var result = MessageBox.Show(
+            //        "Your values are not long enough.", //Message
+            //        "Error", //Title Message
+            //        MessageBoxButtons.OKCancel); //Button
+
+
+            //    if (result == DialogResult.OK)
+            //    {
+            //        MessageBox.Show("Glad you are OK with it.");
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Canciling...");
+            //    }
+            //        return;
+            //}
+            //if (!Validators.IsInteger(txtWidth.Text)||
+            //    !Validators.IsInteger(txtHeight.Text))
+            //{
+            //    MessageBox.Show("Please enter an integer", "Error");
+            //    return;
+            //}
+
+            if (ValidateField(txtWidth.Text, "The width property is too short.")
             {
-                MessageBox.Show("Your values are not long enough.", "Error");
                 return;
             }
-            if (!Validators.IsInteger(txtWidth.Text)||
-                !Validators.IsInteger(txtHeight.Text))
+
+            if (ValidateField(txtHeight.Text, "The height property is too short.")
+            )
             {
-                MessageBox.Show("Please enter an integer", "Error");
                 return;
             }
 
@@ -37,6 +61,21 @@ namespace CalculateArea
 
             // change the total lable to match.
             lblTotal.Text = "Total: " + total.ToString();
+        }
+
+        private bool ValidateField(string field, string errorMessage) 
+        {
+            var result = !Validators.IsMinimumLength(field, 1);
+
+            if (result)
+            {
+                var dialogResult = MessageBox.Show(
+                    errorMessage,
+                    "Error"
+                    );
+            }
+
+            return result;
         }
     }
 }
