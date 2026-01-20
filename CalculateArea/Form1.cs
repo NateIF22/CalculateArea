581126ccdc81068516ab4caf74879584516d23c8
NateIF22 <nathanfisnothere@gmail.com>
2026-01-20 10:19:53 -0800

    style: removed old commented code

--- CalculateArea/Form1.cs
+++ CalculateArea/Form1.cs
@@ -19,32 +19,6 @@ namespace CalculateArea
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            //if (!Validators.IsMinimumLength(txtWidth.Text, 1)||
-            //    !Validators.IsMinimumLength(txtHeight.Text, 1))
-            //{
-            //    var result = MessageBox.Show(
-            //        "Your values are not long enough.", //Message
-            //        "Error", //Title Message
-            //        MessageBoxButtons.OKCancel); //Button
-
-
-            //    if (result == DialogResult.OK)
-            //    {
-            //        MessageBox.Show("Glad you are OK with it.");
-            //    }
-            //    else
-            //    {
-            //        MessageBox.Show("Canciling...");
-            //    }
-            //        return;
-            //}
-            //if (!Validators.IsInteger(txtWidth.Text)||
-            //    !Validators.IsInteger(txtHeight.Text))
-            //{
-            //    MessageBox.Show("Please enter an integer", "Error");
-            //    return;
-            //}
-
             if (ValidateField(txtWidth.Text, "The width property is too short."))
             {
                 return;
